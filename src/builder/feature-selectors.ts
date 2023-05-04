ada50573368d2600891f9ec7b0a8d21ee7268de3
klodian shaba <dev.klodian@gmail.com>
2023-05-04 15:43:36 +0200

    create feature

---
+++ src/builder/feature-selectors.ts
@@ -0,0 +1,16 @@
+import {MemoizedSelector} from "@ngrx/store/src/selector";
+import {EntityAdapter, EntityState, Predicate} from "@ngrx/entity";
+import {createSelector} from "@ngrx/store";
+
+export const featureSelectors = <T, A> (featureSelector: MemoizedSelector<object, object>, featureKey: string) => {
+    const selectState = createSelector<object, object, T & A>(
+        featureSelector,
+        (state) => (featureKey) ? state[featureKey] : state
+    );
+
+    const selectByProperty = (property: string) => createSelector(
+        selectState,
+        (data) => data ? data[property] : data
+    );
+    return {selectState, selectByProperty};
+}
