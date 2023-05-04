68d2906e22c74f8402fda54fc51204fcb1a2100c
klodian shaba <dev.klodian@gmail.com>
2023-05-04 16:06:00 +0200

    removed global effect

--- src/app/store/effects/global.effects.ts
+++ src/app/store/effects/global.effects.ts
@@ -7,20 +7,5 @@ import * as GlobalActions from '../actions/global.actions';
 
 @Injectable()
 export class GlobalEffects {
-
-  loadGlobals$ = createEffect(() => {
-    return this.actions$.pipe(
-
-      // ofType(GlobalActions.loadGlobals),
-      // concatMap(() =>
-      //   /** An EMPTY observable only emits completion. Replace with your own observable API request */
-      //   EMPTY.pipe(
-      //     map(data => GlobalActions.loadGlobalsSuccess({ data })),
-      //     catchError(error => of(GlobalActions.loadGlobalsFailure({ error }))))
-      // )
-    );
-  });
-
-
   constructor(private actions$: Actions) {}
 }
