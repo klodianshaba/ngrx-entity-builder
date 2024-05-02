ec098dec735c07ce29884f5139e5d049b047b6a4
klodian shaba <dev.klodian@gmail.com>
2024-05-02 18:35:18 +0200

    books crud

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -3,28 +3,51 @@ import { booksEntity } from '../../store/entities';
 import { Store } from '@ngrx/store';
 import { State } from '../../store';
 import { BookModel } from '../../models/book.model';
-import { Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatRipple } from '@angular/material/core';
+import { NgClass } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss'],
   standalone: true,
+  imports: [MatButton, MatRipple, NgClass, MatIconButton, MatIcon],
 })
 export class BooksComponent {
   books: Signal<BookModel[] | undefined>;
-  constructor(
-    private store: Store<State>,
-    private router: Router
-  ) {
+  constructor(private store: Store<State>) {
     this.books = toSignal(this.store.select(booksEntity.selectors.selectAll));
   }
-  onBook(book: BookModel) {
-    this.router.navigate(['/book', book.id]).then();
+  updateOne(book: BookModel) {
+    const title = Math.random().toString(16).slice(8);
+    const year =
+      Math.floor(Math.random() * (new Date().getFullYear() - 2000)) + 2000;
+    this.store.dispatch(
+      booksEntity.actions.updateOne({ id: book.id, changes: { title, year } })
+    );
   }
-  onDelete(event: Event, book: BookModel) {
-    event.stopPropagation();
+  removeOne(book: BookModel) {
     this.store.dispatch(booksEntity.actions.removeOne(book.id));
+  }
+  addOne() {
+    const book = this.getRandomBook();
+    this.store.dispatch(booksEntity.actions.addOne(book));
+  }
+  getRandomBook(): BookModel {
+    const id = Number(Math.random());
+    const title = Math.random().toString(16).slice(8);
+    const year =
+      Math.floor(Math.random() * (new Date().getFullYear() - 2000)) + 2000;
+    return {
+      id,
+      title: title,
+      authorId: 1,
+      year: year,
+      pages: 200,
+      rating: 3.5,
+    };
   }
 }
