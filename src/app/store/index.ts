7cdf0b524b31cb314c2f7637beeb6255961cec03
klodian shaba <dev.klodian@gmail.com>
2024-05-02 16:20:32 +0200

    removed authors reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,20 +1,13 @@
 import { isDevMode } from '@angular/core';
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
-import {
-  authorsEntity,
-  authorsFeatureKey,
-  booksEntity,
-  booksFeatureKey,
-} from './entities';
+import { booksEntity, booksFeatureKey } from './entities';
 import { EntityState } from '@ngrx/entity';
 import { BookModel } from '../models/book.model';
-import { AuthorModel } from '../models/author.model';
+
 export interface State {
   [booksFeatureKey]: EntityState<BookModel>;
-  [authorsFeatureKey]: EntityState<AuthorModel>;
 }
 export const reducers: ActionReducerMap<State> = {
   [booksFeatureKey]: booksEntity.reducer,
-  [authorsFeatureKey]: authorsEntity.reducer,
 };
 export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
