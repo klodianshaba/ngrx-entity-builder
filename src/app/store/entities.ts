8108fbec5e4058ceea09648053ce28a2920e34e7
klodian shaba <dev.klodian@gmail.com>
2024-05-28 18:28:12 +0200

    Todo store

--- src/app/store/entities.ts
+++ src/app/store/entities.ts
@@ -1,8 +1,19 @@
 import { createEntity } from '../../builder/create-entity';
+import { entityMethods } from '../../builder/signals/entity-methods';
+import { entityComputed } from '../../builder/signals/entity-computed';
 import { selectState } from './selectors';
 import { BookModel } from '../models/book.model';
+import { TodoModel } from '../models/todo-model';
 import { State } from './index';
 import { AdditionalModel } from '../models/additional.model';
+import { signalStore, withComputed, withMethods } from '@ngrx/signals';
+import { withEntities } from '@ngrx/signals/entities';
+
+export const TodoStore = signalStore(
+  withEntities<TodoModel>(),
+  withComputed(store => entityComputed<TodoModel>(store)),
+  withMethods(store => entityMethods<TodoModel>(store))
+);
 
 export const booksFeatureKey = 'books';
 
